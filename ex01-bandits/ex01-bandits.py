669d30f837c31fff35a2f7c79f0e84b641c80573
JaGesell <jakobgesell@gmail.com>
2021-04-27 12:20:48 +0200

    added UCB1 algorithm

--- ex01-bandits/ex01-bandits.py
+++ ex01-bandits/ex01-bandits.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import random
-
+import math
 
 class GaussianBandit:
     def __init__(self):
@@ -31,7 +31,7 @@ def greedy(bandit, timesteps):
     # iterate over range of bandit arms
     for a in possible_arms:
         # every arm is played once
-        n_plays[a] = 1 # n_plays[a] += 1 is possible as well
+        n_plays[a] = 1  # n_plays[a] += 1 is possible as well
         # get reward by playing arm
         rewards[a] = bandit.play_arm(a)
         # compute sample-average action-value estimates
@@ -41,14 +41,13 @@ def greedy(bandit, timesteps):
     while bandit.total_played < timesteps:
         # TODO: instead do greedy action selection
         # TODO: update the variables (rewards, n_plays, Q) for the selected arm
-        a = np.argmax(Q) #assume there is only one 'best' action because of random selection
+        a = np.argmax(Q)  # assume there is only one 'best' action because of random selection
         # sum up reward for played arm
         rewards[a] += bandit.play_arm(a)
         # increase amount of arm plays by one
         n_plays[a] += 1
         # compute sample-average action-value estimates
         Q[a] = rewards[a] / n_plays[a]
-        
 
 
 def epsilon_greedy(bandit, timesteps):
@@ -64,7 +63,7 @@ def epsilon_greedy(bandit, timesteps):
     # iterate over range of bandit arms
     for a in possible_arms:
         # every arm is played once
-        n_plays[a] = 1 # n_plays[a] += 1 is possible as well
+        n_plays[a] = 1  # n_plays[a] += 1 is possible as well
         # get reward by playing arm
         rewards[a] = bandit.play_arm(a)
         # compute sample-average action-value estimates
@@ -73,31 +72,65 @@ def epsilon_greedy(bandit, timesteps):
     # Main loop
     while bandit.total_played < timesteps:
         var_random = np.random.uniform(0, 1)
-        #probability to take random action: eps 
+        # probability to take random action: eps
         if var_random <= eps:
             # do random action
-            a = random.choice(possible_arms)    
-            
+            a = random.choice(possible_arms)
+
         else:
-            a = np.argmax(Q) #assume there is only one 'best' action because of random selection
-        
+            a = np.argmax(Q)  # assume there is only one 'best' action because of random selection
+
         # sum up reward for played arm
         rewards[a] += bandit.play_arm(a)
         # increase amount of arm plays by one
         n_plays[a] += 1
         # compute sample-average action-value estimates
-        Q[a] = rewards[a] / n_plays[a]            
+        Q[a] = rewards[a] / n_plays[a]
 
+def UCB1(bandit, timesteps):
+    # TODO: epsilon greedy action selection (you can copy your code for greedy as a starting point)
+    rewards = np.zeros(bandit.n_arms)
+    n_plays = np.zeros(bandit.n_arms)
+    Q = np.zeros(bandit.n_arms)
+    U = np.zeros(bandit.n_arms)
+    T = np.zeros(bandit.n_arms)
+    possible_arms = range(bandit.n_arms)
+
+    # TODO: init variables (rewards, n_plays, Q) by playing each arm once
+    # iterate over range of bandit arms
+    for a in possible_arms:
+        # every arm is played once
+        n_plays[a] = 1  # n_plays[a] += 1 is possible as well
+        # get reward by playing arm
+        rewards[a] = bandit.play_arm(a)
+        # compute sample-average action-value estimates
+        Q[a] = rewards[a] / n_plays[a]
+        U[a] = 0
+        T[a] = Q[a] + U[a]
+
+    # Main loop
+    while bandit.total_played < timesteps:
+        a = np.argmax(T)  # assume there is only one 'best' action because of random selection
+
+        # sum up reward for played arm
+        rewards[a] += bandit.play_arm(a)
+        # increase amount of arm plays by one
+        n_plays[a] += 1
+        # compute sample-average action-value estimates
+        Q[a] = rewards[a] / n_plays[a]
+        U[a] = math.sqrt(2*math.log(timesteps)/n_plays[a])
+        T[a] = Q[a] + U[a]
 
 def main():
     n_episodes = 10000  # TODO: set to 10000 to decrease noise in plot
-    n_timesteps = 1000
+    n_timesteps = 3000
     rewards_greedy = np.zeros(n_timesteps)
     rewards_egreedy = np.zeros(n_timesteps)
+    rewards_ucb1 = np.zeros(n_timesteps)
 
     for i in range(n_episodes):
         if i % 100 == 0:
-            print ("current episode: " + str(i))
+            print("current episode: " + str(i))
 
         b = GaussianBandit()  # initializes a random bandit
         greedy(b, n_timesteps)
@@ -107,12 +140,23 @@ def main():
         epsilon_greedy(b, n_timesteps)
         rewards_egreedy += b.rewards
 
+        b.reset()  # reset the bandit before running epsilon_greedy
+        UCB1(b, n_timesteps)
+        rewards_ucb1 += b.rewards
+
     rewards_greedy /= n_episodes
     rewards_egreedy /= n_episodes
+    rewards_ucb1 /= n_episodes
+
     plt.plot(rewards_greedy, label="greedy")
-    print("Total reward of greedy strategy averaged over " + str(n_episodes) + " episodes: " + str(np.sum(rewards_greedy)))
+    print("Total reward of greedy strategy averaged over " + str(n_episodes) + " episodes: " + str(
+        np.sum(rewards_greedy)))
     plt.plot(rewards_egreedy, label="e-greedy")
-    print("Total reward of epsilon greedy strategy averaged over " + str(n_episodes) + " episodes: " + str(np.sum(rewards_egreedy)))
+    print("Total reward of epsilon greedy strategy averaged over " + str(n_episodes) + " episodes: " + str(
+        np.sum(rewards_egreedy)))
+    plt.plot(rewards_ucb1, label="ucb1")
+    print("Total reward of epsilon greedy strategy averaged over " + str(n_episodes) + " episodes: " + str(
+        np.sum(rewards_ucb1)))
     plt.legend()
     plt.xlabel("Timesteps")
     plt.ylabel("Reward")
