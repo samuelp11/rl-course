0ad177b346a42e561b4a9ebf1fad4536c862aa9b
JaGesell <jakobgesell@gmail.com>
2021-05-04 10:27:15 +0200

    own solution, with iterations over itertools.product

--- ex02-mdps/ex02-mdps.py
+++ ex02-mdps/ex02-mdps.py
@@ -1,5 +1,6 @@
 import gym
 import numpy as np
+import itertools
 
 # Init environment
 # Lets use a smaller 3x3 custom map for faster computations
@@ -10,23 +11,24 @@ custom_map3x3 = [
 ]
 env = gym.make("FrozenLake-v0", desc=custom_map3x3)
 # TODO: Uncomment the following line to try the default map (4x4):
-#env = gym.make("FrozenLake-v0")
+# env = gym.make("FrozenLake-v0")
 
 # Uncomment the following lines for even larger maps:
-#random_map = generate_random_map(size=5, p=0.8)
-#env = gym.make("FrozenLake-v0", desc=random_map)
+# random_map = generate_random_map(size=5, p=0.8)
+# env = gym.make("FrozenLake-v0", desc=random_map)
 
 # Init some useful variables:
 n_states = env.observation_space.n
 n_actions = env.action_space.n
 
-r = np.zeros(n_states) # the r vector is zero everywhere except for the goal state (last state)
+r = np.zeros(n_states)  # the r vector is zero everywhere except for the goal state (last state)
 r[-1] = 1.
 
 gamma = 0.8
 
-
 """ This is a helper function that returns the transition probability matrix P for a policy """
+
+
 def trans_matrix_for_policy(policy):
     transitions = np.zeros((n_states, n_states))
     for s in range(n_states):
@@ -37,6 +39,8 @@ def trans_matrix_for_policy(policy):
 
 
 """ This is a helper function that returns terminal states """
+
+
 def terminals():
     terms = []
     for s in range(n_states):
@@ -50,26 +54,36 @@ def value_policy(policy):
     P = trans_matrix_for_policy(policy)
     # TODO: calculate and return v
     # (P, r and gamma already given)
-    return None
-
+    return np.linalg.inv(np.eye(n_states)-gamma*P) @ r
 
 def bruteforce_policies():
     terms = terminals()
     optimalpolicies = []
 
-    policy = np.zeros(n_states, dtype=np.int)  # in the discrete case a policy is just an array with action = policy[state]
+    policy = np.zeros(n_states,
+                      dtype=int)  # in the discrete case a policy is just an array with action = policy[state]
     optimalvalue = np.zeros(n_states)
-    
     # TODO: implement code that tries all possible policies, calculate the values using def value_policy. Find the optimal values and the optimal policies to answer the exercise questions.
+    d = list(range(4))
+    for p in itertools.product(d, d, d, d, d, d, d, d, d):
+        policy = np.array(p)
+        value = value_policy(policy)
+        if (value >= optimalvalue).all():
+            if (value == optimalvalue).all():
+                optimalpolicies.append(np.copy(policy))
+            else:
+                optimalpolicies = [np.copy(policy)]
+                optimalvalue = value
 
-    print ("Optimal value function:")
+
+
+    print("Optimal value function:")
     print(optimalvalue)
-    print ("number optimal policies:")
-    print (len(optimalpolicies))
-    print ("optimal policies:")
-    print (np.array(optimalpolicies))
+    print("number optimal policies:")
+    print(len(optimalpolicies))
+    print("optimal policies:")
+    print(np.array(optimalpolicies))
     return optimalpolicies
-
 
 
 def main():
@@ -79,17 +93,16 @@ def main():
     print("")
 
     # Here a policy is just an array with the action for a state as element
-    policy_left = np.zeros(n_states, dtype=np.int)  # 0 for all states
-    policy_right = np.ones(n_states, dtype=np.int) * 2  # 2 for all states
+    policy_left = np.zeros(n_states, dtype=int)  # 0 for all states
+    policy_right = np.ones(n_states, dtype=int) * 2  # 2 for all states
 
     # Value functions:
     print("Value function for policy_left (always going left):")
-    print (value_policy(policy_left))
+    print(value_policy(policy_left))
     print("Value function for policy_right (always going right):")
-    print (value_policy(policy_right))
+    print(value_policy(policy_right))
 
     optimalpolicies = bruteforce_policies()
-
 
     # This code can be used to "rollout" a policy in the environment:
     """
