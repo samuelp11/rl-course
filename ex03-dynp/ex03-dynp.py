5f35a5554bdcf10ae4a991547ebd22902ed5a371
Samuel Pilch <samuelp11@gmx.de>
2021-05-06 13:09:54 +0200

    basic of value iteration is done...need to implement special case when hole is reached (loop)

--- ex03-dynp/ex03-dynp.py
+++ ex03-dynp/ex03-dynp.py
@@ -17,11 +17,52 @@ n_actions = env.action_space.n
 
 
 def value_iteration():
+    #initialize value function for all , theta and gamma
     V_states = np.zeros(n_states)  # init values as zero
     theta = 1e-8
-    gamma = 0.8
+    gamma = 0.
+    gamma = 0.99
+    
+    #set delta to 1
+    delta = 1e-7
+    #set V_s_max to 0
+    V_s_max = 0 
+    #set counter to 0
+    counter = 0
     # TODO: implement the value iteration algorithm and return the policy
     # Hint: env.P[state][action] gives you tuples (p, n_state, r, is_terminal), which tell you the probability p that you end up in the next state n_state and receive reward r
+    
+    while delta > theta:
+        counter += 1
+        #delta = 0
+        v = V_states.copy()
+        #print(v)
+        for state in range(n_states):
+            V_s_max = 0
+            for action in range(n_actions):
+                V_s = 0
+                #there are either 3 possibilities to continue or 1 if it s the end
+                for i in range(len(env.P[state][action])):
+                    p = env.P[state][action][i][0]
+                    next_state = env.P[state][action][i][1]
+                    r = env.P[state][action][i][2]
+                    V_s = V_s + (r + gamma * p * V_states[next_state])
+                if V_s > V_s_max:
+                    V_s_max = V_s
+                    
+            V_states[state] = V_s_max
+        delta = max(0, np.linalg.norm(v - V_states))
+        if counter == 1:
+            break
+    print(counter)
+    print(v - V_states)
+    print(np.linalg.norm(v - V_states))
+    print(V_states)
+    print(delta)
+    
+    return 0, V_states
+    
+                
 
 
 def main():
@@ -31,7 +72,10 @@ def main():
     print("")
 
     # run the value iteration
-    policy = value_iteration()
+    policy, value_fcn_opt = value_iteration()
+    
+    print("Optimal value function:")
+    print(value_fcn_opt)
     print("Computed policy:")
     print(policy)
 
